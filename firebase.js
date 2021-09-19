911a25d0a56b7180d87584c4589f94328681bfdd
AhmetCihangir <cihangircihangir6@gmail.com>
2021-09-19 16:21:55 +0300

    4.Day Firebase and Auth

--- firebase.js
+++ firebase.js
@@ -1,10 +1,10 @@
 import firebase from "firebase";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyDgX0NawZwMjW6PSp8ATJhTKsgqrgyFV0U",
+    apiKey: process.env.FIREBASE_APIKEY,
     authDomain: "clone-ciryu.firebaseapp.com",
     projectId: "clone-ciryu",
     storageBucket: "clone-ciryu.appspot.com",
-    messagingSenderId: "408269883040",
-    appId: "1:408269883040:web:d336063f99858d670e6bc5"
+    messagingSenderId: process.env.FIREBASE_MESSAGINGSENDERID ,
+    appId: process.env.FIREBASE_APPID
   };
